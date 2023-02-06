eea6ca186f876f1f8370d147276c2e688d18ef35
Evgeniy Batenkov <redlroses.rlr@gmail.com>
2023-02-07 00:05:57 +0500

    Add MonoCache

---
+++ Assets/CodeBase/Debug/TestMonoCash.cs
@@ -0,0 +1,12 @@
+using NTC.Global.Cache;
+
+namespace CodeBase.Debug
+{
+    public class TestMonoCash : MonoCache
+    {
+        protected override void Run()
+        {
+            
+        }
+    }
+}
