f9ae3ca37fcb22bb9afbd06399c10b87f104bc9a
Gorokhov Maksim <GorMax0@users.noreply.github.com>
2023-02-21 16:43:10 +0300

    Create player entity

---
+++ Assets/CodeBase/Logic/Player.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using UnityEngine;
+using NTC.Global.Cache;
+
+namespace CodeBase.Logic
+{
+    public class Player : MonoCache
+    {
+        private int _maxHealth;
+        private int _currentHealth;
+        private bool _isDie;
+
+        public event Action Die;
+
+        public void ReceiveDamage(int damage)
+        {
+            if (damage < 0)
+                throw new InvalidOperationException($"{GetType()}: ReceiveDamage(int damage): invalid value received damage = {damage}");
+
+            _currentHealth -= damage;
+
+            if (_currentHealth <= 0)
+            {
+                _isDie = true;
+                Die?.Invoke();
+            }
+        }
+
+        private void Heal(int health)
+        {
+            if (health < 0)
+                throw new InvalidOperationException($"{GetType()}: Heal(int health): invalid value incoming health = {health}");
+
+            _currentHealth = Mathf.Clamp(_currentHealth + health, 0, _maxHealth);
+        }
+    }
+}
