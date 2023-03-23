d09c42f44934732b6073cc56bfbcd9053b411f21
Evgeniy Batenkov <redlroses.rlr@gmail.com>
2023-03-23 18:44:51 +0500

    Inventory GUI setup

--- Assets/CodeBase/Tools/CustomCanvasScaler.cs
+++ Assets/CodeBase/Tools/CustomCanvasScaler.cs
@@ -1,3 +1,4 @@
+using System.ComponentModel;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -8,19 +9,65 @@ namespace CodeBase.Tools
     {
         [SerializeField] private CanvasScaler _canvas;
 
-        private float _screenWidth;
+        [Space] [Header("Portable")]
+        [SerializeField] private Vector2 _referenceResolutionPortable;
+        [SerializeField] [Range(0f, 1f)] private float _matchWidthOrHeightPortable;
+
+        [Space] [Header("Landscape")]
+        [SerializeField] private Vector2 _referenceResolutionLandscape;
+        [SerializeField] [Range(0f, 1f)] private float _matchWidthOrHeightLandscape;
+
+        private ScreenOrientation _currentOrientation = ScreenOrientation.AutoRotation;
+
+        private ScreenOrientation CurrentOrientation
+        {
+            get => _currentOrientation;
+            set
+            {
+                if (_currentOrientation.Equals(value))
+                {
+                    Debug.Log($"Unchanged: {value}");
+                    return;
+                }
+
+                Debug.Log($"Changed: {value}");
+                _currentOrientation = value;
+                OnOrientationChanged();
+            }
+        }
+
+        private void OnOrientationChanged()
+        {
+            switch (CurrentOrientation)
+            {
+                case ScreenOrientation.Portrait:
+                    SetCanvasParameters(_referenceResolutionPortable, _matchWidthOrHeightPortable);
+                    break;
+                case ScreenOrientation.LandscapeLeft:
+                    SetCanvasParameters(_referenceResolutionLandscape, _matchWidthOrHeightLandscape);
+                    break;
+                default:
+                    throw new InvalidEnumArgumentException(CurrentOrientation.ToString(),
+                        (int) CurrentOrientation,
+                        typeof(ScreenOrientation));
+            }
+        }
+
+        private void SetCanvasParameters(Vector2 resolution, float matchWidthOrHeight)
+        {
+            _canvas.referenceResolution = resolution;
+            _canvas.matchWidthOrHeight = matchWidthOrHeight;
+        }
 
         private void Update()
         {
-            float currentScreenWidth = Screen.width;
+            Resolution currentResolution = Screen.currentResolution;
 
-            if (Mathf.Approximately(_screenWidth, currentScreenWidth))
-            {
-                return;
-            }
+            CurrentOrientation = Screen.width / (float) Screen.height > 1f
+                ? ScreenOrientation.LandscapeLeft
+                : ScreenOrientation.Portrait;
 
-            _screenWidth = currentScreenWidth;
-            _canvas.scaleFactor = 0.00036420395421436004f * currentScreenWidth + 0.0007284079084287201f;
+            Debug.Log(Screen.width / (float) Screen.height);
         }
     }
 }
