b7d298f2d9c265a3d4624559c027f92881a79c31
Evgeniy Batenkov <redlroses.rlr@gmail.com>
2023-04-18 10:41:07 +0500

    Fix loading animation

--- Assets/CodeBase/UI/LoadingAnimation.cs
+++ Assets/CodeBase/UI/LoadingAnimation.cs
@@ -1,4 +1,3 @@
-using TheraBytes.BetterUi;
 using UnityEngine;
 
 namespace CodeBase.UI
@@ -8,17 +7,14 @@ namespace CodeBase.UI
         private const string RotateAnimation = "Rotate";
 
         [SerializeField] private GameObject _rotor;
-        [SerializeField] private LocationAnimations _rotorLocationAnimations;
-
+        
         public void Play()
         {
             _rotor.SetActive(true);
-            _rotorLocationAnimations.StartAnimation(RotateAnimation);
         }
 
         public void Stop()
         {
-            _rotorLocationAnimations.StopCurrentAnimation();
             _rotor.SetActive(false);
         }
     }
