15c044f14cb88e12decbd1f7a1e4c9c7bdc2d549
Evgeniy Batenkov <redlroses.rlr@gmail.com>
2023-04-19 17:43:20 +0500

    Add sound settings wrapper and ISoundService interface

---
+++ Assets/CodeBase/Services/Sound/ISoundService.cs
@@ -0,0 +1,8 @@
+﻿namespace CodeBase.Services.Sound
+{
+    public interface ISoundService : IService
+    {
+        void MusicVolume(float volume);
+        void SoundVolume(float volume);
+    }
+}
---
+++ Assets/CodeBase/UI/Elements/SoundSettings.cs
@@ -0,0 +1,26 @@
+﻿using CodeBase.Services.Sound;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace CodeBase.UI.Elements
+{
+    public class SoundSettings : MonoBehaviour
+    {
+        private ISoundService _soundService;
+        
+        [SerializeField] private Slider _soundSlider;
+        [SerializeField] private Slider _musicSlider;
+
+        public void Construct(ISoundService soundService)
+        {
+            _soundService = soundService;
+            Subscribe();
+        }
+
+        private void Subscribe()
+        {
+            _soundSlider.onValueChanged.AddListener((float volume) => _soundService.SoundVolume(volume));
+            _musicSlider.onValueChanged.AddListener((float volume) => _soundService.MusicVolume(volume));
+        }
+    }
+}
