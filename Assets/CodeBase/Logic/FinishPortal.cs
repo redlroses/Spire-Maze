244a67a83f2069c5082515737ff0d134fd35691a
Evgeniy Batenkov <redlroses.rlr@gmail.com>
2023-04-27 17:32:49 +0500

    Add FinishPortal

---
+++ Assets/CodeBase/Logic/FinishPortal.cs
@@ -0,0 +1,21 @@
+﻿using CodeBase.Logic.Observer;
+using CodeBase.Logic.Portal;
+using CodeBase.UI.Services.Windows;
+
+namespace CodeBase.Logic
+{
+    public class FinishPortal : ObserverTarget<TeleportableObserver, ITeleportable>
+    {
+        private IWindowService _windowService;
+
+        public void Construct(IWindowService windowService)
+        {
+            _windowService = windowService;
+        }
+        
+        protected override void OnTriggerObserverEntered(ITeleportable target)
+        {
+            _windowService.Open(WindowId.Results);
+        }
+    }
+}
