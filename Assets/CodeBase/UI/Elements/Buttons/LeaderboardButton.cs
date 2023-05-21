ed1c0ff3f3fa7e45c4584607a3a24959acf15f4f
Evgeniy Batenkov <redlroses.rlr@gmail.com>
2023-05-21 20:30:43 +0500

    Add UI navigation logic

--- Assets/CodeBase/UI/Elements/Buttons/LeaderboardButton.cs
+++ Assets/CodeBase/UI/Elements/Buttons/LeaderboardButton.cs
@@ -1,9 +1,12 @@
 ﻿using CodeBase.UI.Services.Windows;
+using UnityEngine;
 
 namespace CodeBase.UI.Elements.Buttons
 {
     public class LeaderboardButton : ButtonObserver
     {
+        [SerializeField] private PauseToggle _pauseToggle;
+
         private IWindowService _windowService;
 
         public void Construct(IWindowService windowService)
@@ -15,7 +18,10 @@ namespace CodeBase.UI.Elements.Buttons
         private void OnDestroy() =>
             Cleanup();
 
-        protected override void Call() =>
+        protected override void Call()
+        {
             _windowService.Open(WindowId.Leaderboard);
+            _pauseToggle.EmulateClick();
+        }
     }
 }
--- Assets/CodeBase/UI/Elements/PauseToggle.cs
+++ Assets/CodeBase/UI/Elements/PauseToggle.cs
@@ -8,9 +8,13 @@ namespace CodeBase.UI.Elements
     public class PauseToggle : MonoBehaviour, IPauseWatcher
     {
         [SerializeField] private BetterToggle _pauseToggle;
+        [SerializeField] private LocationAnimations _pauseButtonAnimation;
 
         private IPauseService _pauseService;
         private IWindowService _windowService;
+
+        private void OnDestroy() =>
+            _pauseToggle.onValueChanged.RemoveAllListeners();
 
         public void Construct(IPauseService pauseService, IWindowService windowService)
         {
@@ -19,27 +23,25 @@ namespace CodeBase.UI.Elements
             Subscribe();
         }
 
+        public void EmulateClick() =>
+            _pauseToggle.isOn = !_pauseToggle.isOn;
+
+        public void Resume() =>
+            _pauseToggle.isOn = false;
+
+        public void Pause() =>
+            _pauseToggle.isOn = true;
+
         private void Subscribe() =>
             _pauseToggle.onValueChanged.AddListener(OnPauseToggle);
 
         private void OnPauseToggle(bool isPause)
         {
-            if (_pauseService.IsPause == isPause)
+            if (_pauseService.IsPause)
                 return;
 
             _pauseService.SetPause(isPause);
-
-            if (isPause)
-                _windowService.Open(WindowId.Pause);
+            _windowService.Open(WindowId.Pause);
         }
-
-        private void OnDestroy() =>
-            _pauseToggle.onValueChanged.RemoveAllListeners();
-
-        public void Pause() =>
-            _pauseToggle.isOn = true;
-
-        public void Resume() =>
-            _pauseToggle.isOn = false;
     }
 }
