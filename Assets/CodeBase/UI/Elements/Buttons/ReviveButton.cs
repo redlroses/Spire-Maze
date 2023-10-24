49de34cf135902b1c8fbbbe39bd1a805e9a13beb
Evgeniy Batenkov <redlroses.rlr@gmail.com>
2023-10-24 17:53:09 +0500

    Added revive button

--- Assets/CodeBase/UI/Elements/Buttons/ReviveButton.cs
+++ Assets/CodeBase/UI/Elements/Buttons/ReviveButton.cs
@@ -1,25 +1,45 @@
 ﻿using System;
+using CodeBase.DelayRoutines;
 using CodeBase.Logic.Player;
 using CodeBase.Services.ADS;
+using TheraBytes.BetterUi;
 using UnityEngine;
 
 namespace CodeBase.UI.Elements.Buttons
 {
     public class ReviveButton : ButtonObserver
     {
+        private const string BeatAnimationName = "Beat";
+
+        [Header("Token")]
         [SerializeField] private TextSetter _tokens;
         [SerializeField] private string _format = "X{0}";
+
+        [Space] [Header("Heart Animation")]
+        [SerializeField] private LocationAnimations _locationAnimations;
+        [SerializeField] private float _beatCooldown;
 
         private HeroReviver _reviver;
         private IADService _adService;
         private Action _onRevived;
+        private RoutineSequence _heartAnimation;
+        private LocationAnimations.LocationAnimationEvent _onFinishAnimation;
 
         public void Construct(IADService adService, HeroReviver reviver, Action onRevived = null)
         {
             _onRevived = onRevived ?? (() => { });
             _adService = adService;
             _reviver = reviver;
+            _onFinishAnimation = new LocationAnimations.LocationAnimationEvent(() => _heartAnimation.Play());
+            _heartAnimation = new RoutineSequence().WaitForSeconds(_beatCooldown).Then(HeartBeat);
+            _heartAnimation.Play();
             UpdateTokens();
+            Subscribe();
+        }
+
+        private void HeartBeat()
+        {
+            _locationAnimations.StartAnimation(BeatAnimationName, _onFinishAnimation);
         }
 
         private void UpdateTokens()
