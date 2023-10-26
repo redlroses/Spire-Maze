0e7771efcd58d576b28208fbdba213e690892d43
Evgeniy Batenkov <redlroses.rlr@gmail.com>
2023-10-26 12:00:39 +0500

    Added roll collider reduce

--- Assets/CodeBase/Logic/Movement/Dodge.cs
+++ Assets/CodeBase/Logic/Movement/Dodge.cs
@@ -1,6 +1,7 @@
 using System;
 using CodeBase.Logic.StaminaEntity;
 using CodeBase.Services.Pause;
+using CodeBase.Tools.Extension;
 using NTC.Global.Cache;
 using UnityEngine;
 
@@ -14,6 +15,13 @@ namespace CodeBase.Logic.Movement
         [SerializeField] private PlayerStamina _stamina;
         [SerializeField] private float _invulnerabilityTime = 0.7f;
         [SerializeField] private int _fatigue;
+
+        [Space] [Header("Collider settings")]
+        [SerializeField] private CapsuleCollider _collider;
+        [SerializeField] private float _normalCollideHeight = 1.8f;
+        [SerializeField] private float _normalCollideCenter = 0.98f;
+        [SerializeField] private float _rollCollideHeight = 0.8f;
+        [SerializeField] private float _rollCollideCenter = 0.45f;
 
         private bool _isDodged;
 
@@ -31,12 +39,18 @@ namespace CodeBase.Logic.Movement
         {
             _hitBox.SetActive(false);
             Dodged?.Invoke(direction);
+            _collider.height = _rollCollideHeight;
+            _collider.center = _collider.center.ChangeY(_rollCollideCenter);
             _timer.Restart();
             _timer.Play();
         }
 
-        private void OnTurnOff() =>
+        private void OnTurnOff()
+        {
+            _collider.height = _normalCollideHeight;
+            _collider.center = _collider.center.ChangeY(_normalCollideCenter);
             _hitBox.SetActive(true);
+        }
 
         public void Pause() =>
             enabled = false;
