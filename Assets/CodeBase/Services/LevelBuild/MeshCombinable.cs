09c1b4e13315de4b4f868163c34e7fac68681ce7
Evgeniy Batenkov <redlroses.rlr@gmail.com>
2023-10-31 13:32:33 +0500

    Merge branch 'new-models' into develop
    # Conflicts:
    #	Assets/CodeBase/Logic/DoorEnvironment/Door.cs
    #	Assets/Resources/Prefabs/Cells/Door.prefab
    #	Assets/Scenes/Lobby.unity

---
+++ Assets/CodeBase/Services/LevelBuild/MeshCombinable.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+namespace CodeBase.Services.LevelBuild
+{
+    public class MeshCombinable : MonoBehaviour
+    {
+        [SerializeField] private Renderer _renderer;
+        [SerializeField] private MeshFilter _meshFilter;
+
+        public Renderer Renderer => _renderer;
+
+        public MeshFilter Filter => _meshFilter;
+
+        private void OnValidate()
+        {
+            _renderer ??= GetComponent<Renderer>();
+            _meshFilter ??= GetComponent<MeshFilter>();
+        }
+    }
+}
