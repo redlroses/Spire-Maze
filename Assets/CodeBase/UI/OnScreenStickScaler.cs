c8b78156c7c50057844f85e4288bf2da77f2ff56
Evgeniy Batenkov <redlroses.rlr@gmail.com>
2023-11-22 12:32:37 +0500

    Fixed mobile input scaling

---
+++ Assets/CodeBase/UI/OnScreenStickScaler.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using TheraBytes.BetterUi;
+using UnityEngine;
+using UnityEngine.InputSystem.OnScreen;
+
+namespace CodeBase.UI
+{
+    public class OnScreenStickScaler : MonoBehaviour, IResolutionDependency
+    {
+        private const float ToNormalized = 100f;
+
+        [SerializeField] private OnScreenStick _onScreenStick;
+        [SerializeField] private RectTransform _rect;
+        [SerializeField] private float _defaultMovement;
+
+        private void OnValidate()
+        {
+            _onScreenStick ??= GetComponent<OnScreenStick>();
+            _rect ??= GetComponent<RectTransform>();
+        }
+
+        private void OnEnable()
+        {
+            ScaleMovement();
+        }
+
+        public void OnResolutionChanged()
+        {
+            ScaleMovement();
+        }
+
+        private void ScaleMovement()
+        {
+            _onScreenStick.movementRange = _defaultMovement * _rect.rect.height / ToNormalized;
+        }
+    }
+}
