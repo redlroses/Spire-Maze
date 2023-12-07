1bdb5497657f61fb94dbe28e9c0db837aebc93e8
Evgeniy Batenkov <redlroses.rlr@gmail.com>
2023-12-07 19:22:09 +0500

    Begin implementation of analytic service

---
+++ Assets/CodeBase/Services/Analytics/AnalyticsService.cs
@@ -0,0 +1,24 @@
+﻿using GameAnalyticsSDK;
+using UnityEngine;
+
+namespace CodeBase.Services.Analytics
+{
+    public class AnalyticsService
+    {
+        private const string LevelLabel = "Level_";
+        private const string StarsLabel = "Stars";
+        private const string PlayTimeLabel = "PlayTime";
+        private const string ScoreLabel = "Score";
+
+        public void TrackLevelComplete(int levelId, int stars, int playTime, int score)
+        {
+            SendProgressEvent(levelId, StarsLabel, stars, GAProgressionStatus.Complete);
+        }
+
+        private void SendProgressEvent(int levelId, string starsLabel, int stars, GAProgressionStatus status)
+        {
+            string level = $"{LevelLabel}{levelId}";
+            // GameAnalytics.NewProgressionEvent(status, level,  )
+        }
+    }
+}
