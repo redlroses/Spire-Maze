8e145505eeac243b8cfb68fb176b29a7ad92faac
Evgeniy Batenkov <redlroses.rlr@gmail.com>
2023-12-19 12:46:55 +0500

    KInput key fixed

---
+++ Assets/CodeBase/UI/Elements/CustomOnScreenButton.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.InputSystem.Layouts;
+using UnityEngine.InputSystem.OnScreen;
+using UnityEngine.Serialization;
+
+namespace CodeBase.UI.Elements
+{
+    public class CustomOnScreenButton : OnScreenControl, IPointerDownHandler, IPointerUpHandler
+    {
+        public void OnPointerUp(PointerEventData eventData)
+        {
+            SendValueToControl(0.0f);
+        }
+
+        public void OnPointerDown(PointerEventData eventData)
+        {
+            SendValueToControl(1.0f);
+        }
+
+        [FormerlySerializedAs("m_ControlPath")]
+        [InputControl(layout = "Button")]
+        [SerializeField]
+        private string _controlPath;
+
+        protected override string controlPathInternal
+        {
+            get => _controlPath;
+            set => _controlPath = value;
+        }
+    }
+}
