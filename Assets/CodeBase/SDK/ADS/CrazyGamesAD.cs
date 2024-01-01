b9700cf2de6bf62c3da74c5ed59ae000e639edbd
Evgeniy Batenkov <redlroses.rlr@gmail.com>
2024-01-01 14:59:23 +0500

    Added crazy ads

--- Assets/CodeBase/SDK/ADS/CrazyGamesAD.cs
+++ Assets/CodeBase/SDK/ADS/CrazyGamesAD.cs
@@ -5,14 +5,14 @@ namespace CodeBase.SDK.ADS
 {
     public class CrazyGamesAD : IADProvider
     {
-        public void ShowRewardAd(Action onCompleteCallback = null, Action<string> onDenyCallback = null)
-        {
-            throw new NotImplementedException();
-        }
+        public void ShowRewardAd(Action onCompleteCallback = null, Action<string> onDenyCallback = null) =>
+            CrazyAds.Instance.beginAdBreakRewarded(
+                () => onCompleteCallback?.Invoke(),
+                () => onDenyCallback?.Invoke(string.Empty));
 
-        public void ShowInterstitialAd(Action onCompleteCallback = null, Action<string> onDenyCallback = null)
-        {
-            throw new NotImplementedException();
-        }
+        public void ShowInterstitialAd(Action onCompleteCallback = null, Action<string> onDenyCallback = null) =>
+            CrazyAds.Instance.beginAdBreak(
+                () => onCompleteCallback?.Invoke(),
+                () => onDenyCallback?.Invoke(string.Empty));
     }
 }
